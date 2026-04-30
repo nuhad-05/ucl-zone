25875b80e4f1c02804d329fe2f208ef679396d7a
nuhadnawar <nuhad.nawar@gmail.com>
2026-04-30 16:45:20 -0700

    first commit

---
+++ src/main/java/com/ucl/ucl_zone/UclZoneApplication.java
@@ -0,0 +1,13 @@
+package com.ucl.ucl_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UclZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UclZoneApplication.class, args);
+	}
+
+}
