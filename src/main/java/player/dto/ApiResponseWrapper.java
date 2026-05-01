34aa91968e799d87f6fe8be79cb1d617e2e3ef57
nuhadnawar <nuhad.nawar@gmail.com>
2026-04-30 17:48:53 -0700

    implemented ApiResponseWrapper

--- src/main/java/player/dto/ApiResponseWrapper.java
+++ src/main/java/player/dto/ApiResponseWrapper.java
@@ -1,8 +1,19 @@
 package player.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ApiResponseWrapper {
 
     private List<PlayerEntry> response;
-    
+
+    public List<PlayerEntry> getResponse() {
+        return response;
+    }
+
+    public void setResponse(List<PlayerEntry> response) {
+        this.response = response;
+    }
+
 }
