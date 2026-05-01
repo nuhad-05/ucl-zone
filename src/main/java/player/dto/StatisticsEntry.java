8ffd5fa749c15ce823d281e8d80a8e25f5fbc199
nuhadnawar <nuhad.nawar@gmail.com>
2026-04-30 18:03:29 -0700

    implemented statistics entry

--- src/main/java/player/dto/StatisticsEntry.java
+++ src/main/java/player/dto/StatisticsEntry.java
@@ -1,5 +1,46 @@
 package player.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StatisticsEntry {
+
+    private TeamInfo team;
+    private GamesInfo games;
+    private GoalsInfo goals;
+    private CardsInfo cards;
+
+    public TeamInfo getTeam() {
+        return team;
+    }
+
+    public void setTeam(TeamInfo team) {
+        this.team = team;
+    }
+
+    public GamesInfo getGames() {
+        return games;
+    }
+
+    public void setGames(GamesInfo games) {
+        this.games = games;
+    }
+
+    public GoalsInfo getGoals() {
+        return goals;
+    }
+
+    public void setGoals(GoalsInfo goals) {
+        this.goals = goals;
+    }
+
+    public CardsInfo getCards() {
+        return cards;
+    }
+
+    public void setCards(CardsInfo cards) {
+        this.cards = cards;
+    }
+
 
 }
