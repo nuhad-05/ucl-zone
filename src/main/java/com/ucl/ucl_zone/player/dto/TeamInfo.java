9203d0d97a4a70b6c7ba6b32085039f69e43578c
nuhadnawar <nuhad.nawar@gmail.com>
2026-05-02 03:34:51 -0700

    implemented games info

--- src/main/java/com/ucl/ucl_zone/player/dto/TeamInfo.java
+++ src/main/java/com/ucl/ucl_zone/player/dto/TeamInfo.java
@@ -1,5 +1,18 @@
 package com.ucl.ucl_zone.player.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TeamInfo {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
