166f1e2e8e0104a6c978f678c003c434f3fea7b2
nuhadnawar <nuhad.nawar@gmail.com>
2026-05-03 06:36:20 -0700

    Implemented PlayerRepository interface

--- src/main/java/com/ucl/ucl_zone/player/PlayerRepository.java
+++ src/main/java/com/ucl/ucl_zone/player/PlayerRepository.java
@@ -1,5 +1,15 @@
 package com.ucl.ucl_zone.player;
 
-public class PlayerRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, String> {
+
+    void deleteByname(String playerName);
+
+    Optional<Player> findByName(String name);
 
 }
