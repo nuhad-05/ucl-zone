0774e4209c364a45475a5fa714f7f269e513d843
nuhadnawar <nuhad.nawar@gmail.com>
2026-08-14 07:16:03 -0700

    added checklist if data table already exists then springboot wont fetch and update from api.

--- src/main/java/com/ucl/ucl_zone/DataLoader.java
+++ src/main/java/com/ucl/ucl_zone/DataLoader.java
@@ -1,29 +1,33 @@
 package com.ucl.ucl_zone;
 
+import com.ucl.ucl_zone.player.Player;
+import com.ucl.ucl_zone.player.PlayerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-import com.ucl.ucl_zone.player.Player;
-import com.ucl.ucl_zone.player.PlayerRepository;
-
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final PlayerRepository playerRepository;
     private final ApiFootballService apiFootballService;
+    private final PlayerRepository playerRepository;
 
-    public DataLoader(PlayerRepository playerRepository, ApiFootballService apiFootballService) {
-        this.playerRepository = playerRepository;
+    public DataLoader(ApiFootballService apiFootballService, PlayerRepository playerRepository) {
         this.apiFootballService = apiFootballService;
+        this.playerRepository = playerRepository;
     }
 
-@Override
-public void run(String... args) throws Exception {
-    List<Player> players = apiFootballService.fetchTopTeamPlayers();
-    playerRepository.saveAll(players);
-    System.out.println("Successfully saved " + players.size() + " top club players!");
-}
-
+    @Override
+    public void run(String... args) throws Exception {
+        if (playerRepository.count() == 0) {
+            System.out.println("Database empty. Fetching player data from API-Football...");
+            List<Player> players = apiFootballService.fetchTopTeamPlayers();
+            playerRepository.saveAll(players);
+            System.out.println("Successfully saved " + players.size() + " top club players!");
+        } else {
+            System.out.println("Players already exist in local database (" 
+                    + playerRepository.count() + " found). Skipping API-Football fetch.");
+        }
+    }
 }
